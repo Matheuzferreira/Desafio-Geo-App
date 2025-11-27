42c6d32016500617190ffc730d812c5c1fc6c289
Matheuzferreira <protimatheus@gmail.com>
2025-11-27 00:16:03 -0300

    Comeco

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,12 +1,10 @@
-// src/database.js
+
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const uri = process.env.MONGO_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+
+mongoose.connect(uri) 
 .then(() => console.log('MongoDB conectado!'))
 .catch(err => console.error('Erro ao conectar no MongoDB:', err));
 
